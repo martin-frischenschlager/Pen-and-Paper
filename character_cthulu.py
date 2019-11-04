dcc1fbc342bd7ab0dc13339b24e085993657c1c5
martin-frischenschlager <57159143+martin-frischenschlager@users.noreply.github.com>
2019-11-05 00:20:00 +0100

    code linted for pep8 conform

--- character_cthulu.py
+++ character_cthulu.py
@@ -1,41 +1,53 @@
 # =============================================================================
 # Call of Cthulu - Caracter designer
 # =============================================================================
-#this should be a step by step guid through the character design process
-#the process shall be broken down into sections
-#everything should be well narrated and easily understood
-from cthulu import *
+# this should be a step by step guid through the character design process
+# the process shall be broken down into sections
+# everything should be well narrated and easily understood
+from cthulu import Character, intInput, agemodify
 
-print("########  Welcome to the \"Call of Cthulu\"-Character-Designer!  ########")
+greeting = (
+    "########  Welcome to the \"Call of Cthulu\"-"
+    "Character-Designer!  ########")
+print(greeting)
 
 playedBy = input("First up, tell me YOUR NAME: ")
 
-print("Hi "+playedBy+"! Let's begin setting up your new character!")
+print("Hi " + playedBy + "! Let's begin setting up your new character!")
 
 name = input("Now, what NAME do you give your new character?: ")
 player = Character(name, playedBy)
 
-print("Alright,",playedBy+"! Let's start by determining",name+"'s stats!")
+print("Alright,", playedBy +
+      "! Let's start by determining", name + "'s stats!")
 print("First up, do you want to roll dice yourself?")
 
-#self or automatic dice throwing?
+# self or automatic dice throwing?
 while True:
     roll = input("[Y/N] ")
-    if roll == 'Y': roll = True; print("Alright, get your dice ready, here we go!"); break
-    elif roll == 'N': roll = False; print("Alright, here we go!"); break
-    else: print("Sorry, I didn't recognize that, try again.")
+    if roll == 'Y':
+        roll = True
+        print("Alright, get your dice ready, here we go!")
+        break
+    elif roll == 'N':
+        roll = False
+        print("Alright, here we go!")
+        break
+    else:
+        print("Sorry, I didn't recognize that, try again.")
 
-#initialize stats
+# initialize stats
 if roll:
     pass
 else:
     player.init_stats()
-    print("Here are",name+"'s preliminary stats.")
+    print("Here are", name + "'s preliminary stats.")
     for key, value in player.stats.items():
-        if key == 'MOV': continue
-        print(key+":",value)
+        if key == 'MOV':
+            continue
+        print(key + ":", value)
 
-age = intInput(15,90,"Age","Now, what AGE is "+name+"? (15-90): ")
+age = intInput(15, 90, "Age", "Now, what AGE is " + name + "? (15-90): ")
 
 luckBonus = False
 eduBonus = 0
@@ -43,52 +55,61 @@ if roll:
     pass
 else:
     if 15 <= age and age < 20:
-        print("Alright, then we need to remove a total of 5 points from STR and SIZ as well as 5 points from EDU.")
+        msg = ("Alright, then we need to remove a total of 5 points from "
+               "STR and SIZ as well as 5 points from EDU.")
+        print(msg)
         print("But you get a bonus throw for luck afterwards.")
         luckBonus = True
-        minus = int(input("How many do you want me to remove from STR? (0-5) "))
+        minus = intInput(0, 5, "Value",
+                         "How many do you want me to remove from STR? (0-5) ")
         player.stats['STR'] -= minus
-        player.stats['SIZ'] -= 5-minus
+        player.stats['SIZ'] -= 5 - minus
         player.stats['EDU'] -= 5
         print("The new values are:")
         for key, value in player.stats.items():
-            if key not in {'STR','SIZ','EDU'}: continue
-            print(key+": ",value)
+            if key not in {'STR', 'SIZ', 'EDU'}:
+                continue
+            print(key + ": ", value)
     elif 20 <= age and age < 40:
         print("You get one chance at a better EDU stat")
         eduBonus = 1
     elif 40 <= age and age < 50:
         eduBonus = 2
-        agemodify(player,5,5,eduBonus)
+        agemodify(player, 5, 5, eduBonus)
         print("The new values are:")
         for key, value in player.stats.items():
-            if key not in {'STR','CON','DEX','APP'}: continue
-            print(key+": ",value)
+            if key not in {'STR', 'CON', 'DEX', 'APP'}:
+                continue
+            print(key + ": ", value)
     elif 50 <= age and age < 60:
         eduBonus = 3
-        agemodify(player,10,10,eduBonus)
+        agemodify(player, 10, 10, eduBonus)
         print("The new values are:")
         for key, value in player.stats.items():
-            if key not in {'STR','CON','DEX','APP'}: continue
-            print(key+": ",value)
+            if key not in {'STR', 'CON', 'DEX', 'APP'}:
+                continue
+            print(key + ": ", value)
     elif 60 <= age and age < 70:
         eduBonus = 4
-        agemodify(player,20,15,eduBonus)
+        agemodify(player, 20, 15, eduBonus)
         print("The new values are:")
         for key, value in player.stats.items():
-            if key not in {'STR','CON','DEX','APP'}: continue
-            print(key+": ",value)
+            if key not in {'STR', 'CON', 'DEX', 'APP'}:
+                continue
+            print(key + ": ", value)
     elif 70 <= age and age < 80:
         eduBonus = 4
-        agemodify(player,40,20,eduBonus)
+        agemodify(player, 40, 20, eduBonus)
         print("The new values are:")
         for key, value in player.stats.items():
-            if key not in {'STR','CON','DEX','APP'}: continue
-            print(key+": ",value)
+            if key not in {'STR', 'CON', 'DEX', 'APP'}:
+                continue
+            print(key + ": ", value)
     elif 80 <= age and age <= 90:
         eduBonus = 4
-        agemodify(player,80,25,eduBonus)
+        agemodify(player, 80, 25, eduBonus)
         print("The new values are:")
         for key, value in player.stats.items():
-            if key not in {'STR','CON','DEX','APP'}: continue
-            print(key+": ",value)
+            if key not in {'STR', 'CON', 'DEX', 'APP'}:
+                continue
+            print(key + ": ", value)
--- cthulu.py
+++ cthulu.py
@@ -1,79 +1,100 @@
 import random
 
+
 class Character:
     """
     An investigator-charakter in the "Call of Cthulu"-Universe
     """
-    
-    #INFO
-    info = {'occupation':'', 'age':'', 'birthday':'', 'sex':'', 'residence':'', 'birthplace':''}
-    
-    #ATTRIBUTES
-    stats =  {'STR':0, 'DEX':0, 'POW':0, 'CON':0, 'APP':0, 'EDU':0, 'SIZ':0, 'INT':0, 'MOV':0}
-    
-    #DERIVED ATTRIBUTES
-    der_stats = {'HP':0, 'sanity':0, 'MP':0, 'luck':0}
-    
+
+    # INFO
+    info = {'occupation': '', 'age': '', 'birthday': '',
+            'sex': '', 'residence': '', 'birthplace': ''}
+
+    # ATTRIBUTES
+    stats = {'STR': 0, 'DEX': 0, 'POW': 0, 'CON': 0, 'APP': 0, 'EDU': 0,
+             'SIZ': 0, 'INT': 0, 'MOV': 0}
+
+    # DERIVED ATTRIBUTES
+    der_stats = {'HP': 0, 'sanity': 0, 'MP': 0, 'luck': 0}
+
     def __init__(self, name, playedBy):
         self.info['name'] = name
         self.info['playedBy'] = playedBy
-        
+
     def init_stats(self):
         print("setting attributes")
         for key, value in self.stats.items():
             if key not in {'SIZ', 'INT', 'EDU', 'MOV'}:
-                self.stats[key] = W(6,3,5)
+                self.stats[key] = W(6, 3, 5)
             elif key != 'MOV':
-                self.stats[key] = W(6,2,5,6)
+                self.stats[key] = W(6, 2, 5, 6)
 
         print("stats \\MOV are set")
         print(self.stats)
-    
-        
-def W(faces=100,throws=1, mult=1, add=0):
-    """ default returns a 100-sided dicethrow-result, 
+
+
+def W(faces=100, throws=1, mult=1, add=0):
+    """ default returns a 100-sided dicethrow-result,
     options include No. of faces, No. of summmed throws,
-    multiplicator of result, offset addition to the result """
+    multiplicator of result, offset addition to the result
+    """
     list = []
     for i in range(throws):
-        list.append(random.randint(1,faces))
-    return (sum(list)+add)*mult
+        list.append(random.randint(1, faces))
+    return (sum(list) + add) * mult
 
-def intInput(a,b,inputname="Input",inputtext=""):
-    """ asks for an Integer-input in [a,b], returns input once correctly entered """
+
+def intInput(a, b, inputname="Input", inputtext=""):
+    """ asks for an Integer-input in [a,b], returns input once correctly entered
+    """
     while True:
         inp = input(inputtext)
         try:
             inp_int = int(inp)
-            if not a <= inp_int <= b: print(inputname,"must be between",a,"and",b); raise ValueError 
+            if not a <= inp_int <= b:
+                print(inputname, "must be between", a, "and", b)
+                raise ValueError
             break
-        except: print("Please try again!")
+        except Exception:
+            print("Please try again!")
     return inp_int
 
-def agemodify(self,subtr,appmin,edubon):
+
+def agemodify(self, subtr, appmin, edubon):
     """ Modifies the STR,CON,DEX-stats according to age """
-    
-    statsum = sum((self.stats['STR'],self.stats['CON'],self.stats['DEX']))
-    print("Alright, then we need to remove a total of",subtr,"points from STR and CON and DEX as well as",appmin,"points from APP.")
-    print("BUT: You get",edubon,"chances at a better EDU stat.")
-    
-    self.stats['APP'] -= min(self.stats['APP'],appmin)
-        
+    statsum = sum((self.stats['STR'], self.stats['CON'], self.stats['DEX']))
+    print("Alright, then we need to remove a total of", subtr, "points from "
+          "STR and CON and DEX as well as", appmin, "points from APP.")
+    print("BUT: You get", edubon, "chances at a better EDU stat.")
+
+    self.stats['APP'] -= min(self.stats['APP'], appmin)
+
     if subtr >= statsum:
-        print("Sum of stats smaller than or equal", subtr, "all three stats set to zero.")
-        self.stats['STR'] = 0; self.stats['CON'] = 0; self.stats['DEX'] = 0
+        print("Sum of stats smaller than or equal", subtr, "all three stats "
+              "set to zero.")
+        self.stats['STR'] = 0
+        self.stats['CON'] = 0
+        self.stats['DEX'] = 0
         return
-    
-    lower = max(0,subtr + self.stats['STR'] - statsum)
-    upper = min(subtr,self.stats['STR'])
-    min1 = intInput(lower,upper,"Value","How many points do you want me to remove from STR? ("+str(lower)+"-"+str(upper)+") ")
-    self.stats['STR'] -= min1; subtr -= min1
-    
-    if subtr == (self.stats['CON']+self.stats['DEX']):
+
+    lower = max(0, subtr + self.stats['STR'] - statsum)
+    upper = min(subtr, self.stats['STR'])
+    min1 = intInput(lower, upper, "Value",
+                    "How many points do you want me to remove from STR? "
+                    "(" + str(lower) + "-" + str(upper) + ") ")
+    self.stats['STR'] -= min1
+    subtr -= min1
+
+    if subtr == (self.stats['CON'] + self.stats['DEX']):
         print("Remaining stats set to zero.")
-        self.stats['CON'] = 0; self.stats['DEX'] = 0
-    lower = max(0,subtr + self.stats['STR'] + self.stats['CON'] + min1 - statsum)
-    upper = min(subtr,self.stats['DEX'])
-    min2 = intInput(lower,upper,"Value","How many points from CON? ("+str(lower)+"-"+str(upper)+") ")
-    self.stats['CON'] -= min2; subtr -= min2
+        self.stats['CON'] = 0
+        self.stats['DEX'] = 0
+    lower = max(0, subtr + self.stats['STR'] + self.stats['CON'] + min1
+                - statsum)
+    upper = min(subtr, self.stats['DEX'])
+    min2 = intInput(lower, upper, "Value",
+                    "How many points from CON? ("
+                    + str(lower) + "-" + str(upper) + ") ")
+    self.stats['CON'] -= min2
+    subtr -= min2
     self.stats['DEX'] -= subtr
